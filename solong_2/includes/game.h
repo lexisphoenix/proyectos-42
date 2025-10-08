46f38ed5621b59d204674545da9b82e8fc34dcac
Alejandro Javier Nieto Munoz <anieto-m@c2r3s3.42malaga.com>
2025-10-08 14:52:54 +0200

    corregido norminette

--- solong_2/includes/game.h
+++ solong_2/includes/game.h
@@ -5,57 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anieto-m <anieto-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/06 12:53:43 by anieto-m          #+#    #+#             */
-/*   Updated: 2025/10/07 16:52:36 by anieto-m         ###   ########.fr       */
+/*   Created: 2025/09/11 15:22:24 by anieto-m          #+#    #+#             */
+/*   Updated: 2025/10/08 14:49:15 by anieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAME_H
 # define GAME_H
 # include "map.h"
-# include "mlx.h"
+# include "../minilibx/linux/mlx.h"
 
 typedef struct s_game
 {
-    void    *mlx;
-    void    *win;
-    void    *img;
-    char    *addr;
-    int     bpp;
-    int     sl;
-    int     endian;
-    int     ts;
-    int     w;
-    int     h;
-    char    **grid;
-    int     px;
-    int     py;
-    int     remaining_c;
-    int     moves;
-    void    *img_wall;
-    void    *img_floor;
-    void    *img_exit;
-    void    *img_exit_flipped;
-    int     exit_tick;
-    int     exit_flip;
-    void    *img_coin[2];
-    int     coin_frame;
-    int     coin_tick;
-    void    *img_player_iz[4];
-    void    *img_player_der[4];
-    int     player_frame;
-    int     dir;
-    int     idle_tick;
-    int     idle_state;
-    int     tx;
-    int     ty;
-}   t_game;
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		sl;
+	int		endian;
+	int		ts;
+	int		w;
+	int		h;
+	char	**grid;
+	int		px;
+	int		py;
+	int		remaining_c;
+	int		moves;
+	void	*img_wall;
+	void	*img_floor;
+	void	*img_exit;
+	void	*img_exit_flipped;
+	int		exit_tick;
+	int		exit_flip;
+	void	*img_coin[2];
+	int		coin_frame;
+	int		coin_tick;
+	void	*img_player_iz[4];
+	void	*img_player_der[4];
+	int		player_frame;
+	int		dir;
+	int		idle_tick;
+	int		idle_state;
+	int		tx;
+	int		ty;
+}		t_game;
 
-int     game_start(t_map *m);
-void    render(t_game *g);
-int     loop_animation(t_game *g);
-int     try_move(t_game *g, int dx, int dy);
-int     on_key_press(int key, t_game *g);
-int     on_window_close(t_game *g);
+int		game_start(t_map	*m);
+void	render(t_game	*g);
+int		loop_animation(t_game	*g);
+int		try_move(t_game *g, int dx, int dy);
+int		on_key_press(int key,	t_game	*g);
+int		on_window_close(t_game	*g);
 
 #endif
--- solong_2/includes/map.h
+++ solong_2/includes/map.h
@@ -6,7 +6,7 @@
 /*   By: anieto-m <anieto-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 15:22:24 by anieto-m          #+#    #+#             */
-/*   Updated: 2025/09/11 19:16:29 by anieto-m         ###   ########.fr       */
+/*   Updated: 2025/10/08 14:52:01 by anieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 
 typedef struct s_map
 {
-    char    **grid;
-    int     w;
-    int     h;
-    int     px;
-    int     py;
-    int     count_p;
-    int     count_c;
-    int     count_e;
-}   t_map;
+	char	**grid;
+	int		w;
+	int		h;
+	int		px;
+	int		py;
+	int		count_p;
+	int		count_c;
+	int		count_e;
+}	t_map;
 
 typedef struct s_queue
 {
-	int x;
-	int y;
-}   t_queue;
+	int	x;
+	int	y;
+}	t_queue;
 
 typedef struct s_validation_data
 {
@@ -47,11 +47,11 @@ typedef struct s_pathfinding_data
 	int				reached_exit;
 }	t_pathfinding_data;
 
-int     parse_map(const char *path, t_map *m);
-void    free_map(t_map *m);
-int     is_rectangular(t_map *m);
-int     closed_by_walls(t_map *m);
-int     validate_characters_and_counts(t_map *m);
-int     check_path(t_map *m);
+int		parse_map(const char *path, t_map *m);
+void	free_map(t_map *m);
+int		is_rectangular(t_map *m);
+int		closed_by_walls(t_map *m);
+int		validate_characters_and_counts(t_map *m);
+int		check_path(t_map *m);
 
 #endif
