6e57aa58d18ed9dc2f07e15132c842654ca697df
Alejandro Javier Nieto Munoz <anieto-m@c2r9s3.42malaga.com>
2025-10-15 19:55:14 +0200

    a

--- solong_2/includes/game.h
+++ solong_2/includes/game.h
@@ -6,7 +6,7 @@
 /*   By: anieto-m <anieto-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 15:22:24 by anieto-m          #+#    #+#             */
-/*   Updated: 2025/10/08 14:49:15 by anieto-m         ###   ########.fr       */
+/*   Updated: 2025/10/15 19:52:59 by anieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,38 @@
 
 typedef struct s_game
 {
-	void	*mlx;
-	void	*win;
-	void	*img;
-	char	*addr;
-	int		bpp;
-	int		sl;
-	int		endian;
-	int		ts;
-	int		w;
-	int		h;
-	char	**grid;
-	int		px;
-	int		py;
-	int		remaining_c;
-	int		moves;
-	void	*img_wall;
-	void	*img_floor;
-	void	*img_exit;
-	void	*img_exit_flipped;
-	int		exit_tick;
-	int		exit_flip;
-	void	*img_coin[2];
-	int		coin_frame;
-	int		coin_tick;
-	void	*img_player_iz[4];
-	void	*img_player_der[4];
-	int		player_frame;
-	int		dir;
-	int		idle_tick;
-	int		idle_state;
-	int		tx;
-	int		ty;
+	void	*mlx; // mlx instance
+	void	*win; 	// window
+	void	*img; // current image
+	char	*addr; // image address
+	int		bpp; // bits per pixel
+	int		sl; // size line
+	int		endian; // endianess
+	int		ts; // tile size
+	int		w; // map width
+	int		h;	// map height
+	char	**grid; 	// map grid
+	int		px; 	
+	int		py;	 // player position
+	int		remaining_c; 	// remaining coins
+	int		moves; 	// move count
+	void	*img_wall; // wall image
+	void	*img_floor; // floor image
+	void	*img_exit; 	// exit image
+	void	*img_exit_flipped; // exit image when flipped
+	int		exit_tick; // exit animation tick
+	int		exit_flip; // exit flip state
+	void	*img_coin[2]; // coin images for animation
+	int		coin_frame; // current coin frame
+	int		coin_tick; 	// coin animation tick
+	void	*img_player_iz[4]; // player left images 
+	void	*img_player_der[4];	 // player right images 
+	int		player_frame;	 // current player frame 
+	int		dir; // player direction (0 left, 1 right)
+	int		idle_tick; // idle animation tick
+	int		idle_state; // idle animation state
+	int		tx;	// target x for movement animation
+	int		ty; // target y for movement animation
 }		t_game;
 
 int		game_start(t_map	*m);
